5868726efaa919e50831320ec406f057f7d3bba5
cheex0r <cheex0r@gmail.com>
2023-06-23 17:22:49 -0700

    Create module for launching Steam.
    Steam location is hardcoded.

---
+++ SteamLauncher/Modules/Launcher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using System.ComponentModel;
+
+namespace SteamLauncher.Modules
+{
+    internal class Launcher
+    {
+
+        public static void Launch()
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = "C:\\Program Files (x86)\\Steam\\steam.exe";
+            process.Start();
+        }
+    }
+}
